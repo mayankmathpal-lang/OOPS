63755eeec0837ac8f4c4b7502f5198587e3b4b4c
Mayank Mathpal <mayankmathpal939@gmail.com>
2026-03-10 13:13:27 +0530

    first commit

---
+++ Mayank/launchstatic6.java
@@ -0,0 +1,27 @@
+package Mayank;
+
+ class Demo4 {
+
+	
+	
+int a=10;
+	
+{
+	disp();
+	
+}
+	
+	void disp() {
+		
+		System.out.println("a "+a);
+			}
+}
+public class launchstatic6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+          Demo4 d = new Demo4();
+          }
+}
+
+
